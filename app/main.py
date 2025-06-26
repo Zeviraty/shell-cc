5986c7880d7a42ab887f6243149df7687ccef23a
zeviraty <zeviraty@gmail.com>
2025-06-26 15:57:18 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -104,15 +104,10 @@ def complete(text, state):
                 continue
         complete._cmds = cmds
 
-    results = [x + " " for x in [*complete._cmds, *builtins] if x.startswith(text)] + [None]
+    results = [x for x in [*complete._cmds, *builtins] if x.startswith(text)] + [None]
     if results == [None]:
         print("\x07")
     return results[state]
-
-def display_completions(substitution, matches, longest_match_length):
-    print("\n" + " ".join(matches))  # two spaces
-    sys.stdout.write(readline.get_line_buffer())
-    sys.stdout.flush()
 
 global builtins
 builtins = ["exit","echo","type","pwd"]
