eb44a422648069db2cacfc0a9a969678b5af90ca
zeviraty <zeviraty@gmail.com>
2025-06-26 17:21:51 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -126,6 +126,7 @@ builtins = ["exit","echo","type","pwd","history"]
 
 def main():
     if os.path.exists(os.environ["HISTFILE"]):
+        print(os.environ["HISTFILE"])
         readline.read_history_file(os.environ["HISTFILE"])
     initial_history_length = readline.get_current_history_length()
     readline.set_auto_history(False)
